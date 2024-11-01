0c2a359e6f9c1f2964084d963e6de52cbfb4f8c6
Nasache <magiccreator19@gmail.com>
2024-11-01 15:49:06 -0400

    Fixed issue where gilded oak saplings were registered twice

--- src/main/java/net/nathan/barklings/BarklingsMain.java
+++ src/main/java/net/nathan/barklings/BarklingsMain.java
@@ -9,7 +9,6 @@ import net.nathan.barklings.entity.ModEntities;
 import net.nathan.barklings.item.ModItemGroup;
 import net.nathan.barklings.item.ModItems;
 import net.nathan.barklings.sound.ModSounds;
-import net.nathan.barklings.util.ModLootTableModifiers;
 import net.nathan.barklings.util.ModRegistries;
 import net.nathan.barklings.world.gen.ModWorldGeneration;
 import org.slf4j.Logger;
@@ -29,8 +28,6 @@ public class BarklingsMain implements ModInitializer {
 		ModRegistries.registerModStuffs();
 
 		ModEntities.registerModEntities();
-
-		ModLootTableModifiers.modifyLootTables();
 
 		ModWorldGeneration.generateModWorldGeneration();
 
