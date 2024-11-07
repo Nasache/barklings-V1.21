04666e851647a56e22a5c78e841ab54c5fdda96d
Nasache <magiccreator19@gmail.com>
2024-11-07 14:15:24 -0500

    Fixed berry bushes and translations

--- src/main/java/net/nathan/barklings/datagen/ModBlockTagProvider.java
+++ src/main/java/net/nathan/barklings/datagen/ModBlockTagProvider.java
@@ -61,7 +61,11 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
                         ModBlocks.STRIPPED_GILDED_OAK_WOOD);
 
         getOrCreateTagBuilder(ModTags.Blocks.GILDED_OAK_LOGS)
-                .add(ModBlocks.GILDED_OAK_LOG, ModBlocks.GILDED_OAK_WOOD, ModBlocks.STRIPPED_GILDED_OAK_LOG, ModBlocks.STRIPPED_GILDED_OAK_WOOD);
+                .add(ModBlocks.GILDED_OAK_LOG, ModBlocks.GILDED_OAK_WOOD,
+                        ModBlocks.STRIPPED_GILDED_OAK_LOG, ModBlocks.STRIPPED_GILDED_OAK_WOOD);
+
+        getOrCreateTagBuilder(BlockTags.FLOWERS)
+                .add(ModBlocks.CLOVER);
 
     }
 }
--- src/main/java/net/nathan/barklings/mixin/SweetBerryBushBlockMixin.java
+++ src/main/java/net/nathan/barklings/mixin/SweetBerryBushBlockMixin.java
@@ -1,32 +1,24 @@
 package net.nathan.barklings.mixin;
 
-
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SweetBerryBushBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.nathan.barklings.entity.custom.BarklingEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SweetBerryBushBlock.class)
 public abstract class SweetBerryBushBlockMixin {
 
-    @Overwrite
-    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
-        if (entity instanceof LivingEntity && !(entity instanceof net.minecraft.entity.passive.FoxEntity) && !(entity instanceof net.minecraft.entity.passive.BeeEntity) && !(entity instanceof BarklingEntity)) {
-            entity.slowMovement(state, new Vec3d(0.800000011920929, 0.75, 0.800000011920929));
-            if (!world.isClient && (Integer)state.get(SweetBerryBushBlock.AGE) > 0 &&
-                    (entity.lastRenderX != entity.getX() || entity.lastRenderZ != entity.getZ())) {
-                double d = Math.abs(entity.getX() - entity.lastRenderX);
-                double e = Math.abs(entity.getZ() - entity.lastRenderZ);
-                if (d >= 0.003000000026077032 || e >= 0.003000000026077032) {
-                    entity.damage(world.getDamageSources().sweetBerryBush(), 1.0F);
-                }
-            }
+    @Inject(method = "onEntityCollision", at = @At("HEAD"), cancellable = true)
+    private void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity, CallbackInfo info) {
+        if (entity instanceof LivingEntity && (entity instanceof BarklingEntity)) {
+            info.cancel();
         }
     }
 }
