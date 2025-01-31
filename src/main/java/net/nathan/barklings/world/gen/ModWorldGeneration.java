e4b9703fd32d166f8ccde424d6097f7d0785d416
Nasache <magiccreator19@gmail.com>
2025-01-31 14:56:19 -0500

    Started work on hermit houses

--- src/main/java/net/nathan/barklings/world/gen/ModWorldGeneration.java
+++ src/main/java/net/nathan/barklings/world/gen/ModWorldGeneration.java
@@ -3,6 +3,5 @@ package net.nathan.barklings.world.gen;
 public class ModWorldGeneration {
     public static void generateModWorldGeneration() {
         ModFlowerGeneration.generateFlowers();
-        ModEntitySpawns.addSpawns();
     }
 }
